7b275cd73c289d6d705f0c8c0a3f0002b289589a
01vineetkumar <01vineetkumar@gmail.com>
2022-01-29 22:43:27 +0530

    initial

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -62,9 +62,11 @@ function Home() {
             <header className="App-header">
                 {!detailView && <>
                     <h1>Warehouses</h1>
-                    <p style={{fontSize:"medium"}}>List of Warehouses currently saved in our System</p>
+                    <p style={{ fontSize: "medium" }}>List of Warehouses currently saved in our System</p>
                     <div className="controls">
-                        <input className="searchBox" type="text" placeholder="Search here" onKeyUp={(e) => { search(e.target.value); }} />
+                        <div className="flexBox">
+                            <input className="searchBox" type="text" placeholder="Search here" onKeyUp={(e) => { search(e.target.value); }} />
+                        </div>
                         <div className="flexBox">
 
                             <select onChange={(e) => { searchCity(e.target.value) }} className="searchBox">
@@ -94,27 +96,27 @@ function Home() {
                         })}
                     </ul>
                 </>}
-                {detailView && <div className="detailView" style={{background:"#fff",padding:"25px", boxShadow:"0 0 5px 5px lightgray"}}>
+                {detailView && <div className="detailView" style={{ background: "#fff", margin:"10px", padding: "25px", boxShadow: "0 0 5px 5px lightgray" }}>
                     <h1>{currObj.name}</h1>
                     <h3>Details</h3>
-                    <p style={{fontSize:"medium"}}>Warehouse details are given below. You can edit them and click on save to modify.</p>
-                    <div className="flexBox" >
+                    <p style={{ fontSize: "medium" }}>Warehouse details are given below. You can edit them and click on save to modify.</p>
+                    <div className="flexBox flex-Mv" >
                         <div>
                             <h5>Name</h5>
-                            <input type="text" onKeyUp={(e) => { currObj.name = e.target.value; }} placeholder={currObj.name} className="searchBox" />
+                            <input style={{margin:0}} type="text" onKeyUp={(e) => { currObj.name = e.target.value; }} placeholder={currObj.name} className="searchBox" />
                         </div>
 
                         <div>
                             <h5>City</h5>
-                            <input type={"text"} onKeyUp={(e) => { currObj.city = e.target.value }} placeholder={currObj.city} className="searchBox" />
+                            <input style={{margin:0}} type={"text"} onKeyUp={(e) => { currObj.city = e.target.value }} placeholder={currObj.city} className="searchBox" />
                         </div>
                         <div>
                             <h5>Space available</h5>
-                            <input type={"text"} onKeyUp={(e) => { currObj.space_available = e.target.value }} placeholder={currObj.space_available} className="searchBox" />
+                            <input style={{margin:0}} type={"text"} onKeyUp={(e) => { currObj.space_available = e.target.value }} placeholder={currObj.space_available} className="searchBox" />
                         </div>
                     </div>
-                    <br/>
-                    <button onClick={(e) => { save(currObj.id) }} className="searchBox" >Save Details</button>
+                    <br />
+                    <button style={{width:"fit-content"}} onClick={(e) => { save(currObj.id) }} className="searchBox" >Save Details</button>
                 </div>}
             </header>
         </div>
